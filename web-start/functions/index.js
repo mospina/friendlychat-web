62b4e7d9b18a666afa03d6b901a56f437df4cc36
Manuel Ospina <manuel@anect.jp>
2019-12-10 14:23:01 +0900

    Complete codelab Cloud Functions for firebase

--- web-start/functions/index.js
+++ web-start/functions/index.js
@@ -14,12 +14,80 @@
  * limitations under the License.
  */
 
-// Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
+// Import the Cloud Functions for Firebase.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin')
+admin.initializeApp();
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(
+  async (user) => {
+    console.log('A new user signed in for the first time.');
+    const fullName = user.displayName || 'Anonymous';
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+    // Saves the new welcome message into the database
+    // which then displays it in the FriendlyChat clients
+    await admin.firestore().collection('messages').add({
+      name: 'Firebase Bot',
+      profilePicUrl: '/images/firebase-logo.png',
+      text: `${fullName} signed in for the first time! Welcome!`,
+      timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    });
+    console.log('Welcome message written to database');
+  }
+);
+
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
+// Skipped as this part of the lab needs to enable billing.  
+// https://codelabs.developers.google.com/codelabs/firebase-cloud-functions/#8
 
-// TODO(DEVELOPER): Write the sendNotifications Function here.
+// Send a notification to all users when a new message is posted.
+exports.sendNotifications = functions.firestore.document('messages/{messageId}').onCreate(
+  async (snapshot) => {
+    // Notification details
+    const text = snapshot.data().text;
+    const payload = {
+      notification: {
+        title: `${snapshot.data().name} posted ${text ? 'a message' : 'an image'}`,
+        body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
+        icon: snapshot.data().profilePicUrl || '/images/profile_placeholder.png',
+        click_action: `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com`,
+      }
+    };
+
+    // Get the list of device tokens.
+    const allTokens = await admin.firestore().collection('fcmTokens').get();
+    const tokens = [];
+    allTokens.forEach((tokenDoc) => {
+      tokens.push(tokenDoc.id);
+    });
+
+    if (tokens.length > 0) {
+      // send notification to all tokens
+      const response = await admin.messaging().sendToDevice(tokens, payload);
+      await cleanUpTokens(response, tokens);
+      console.log('Notifications have been sent and tokens cleaned up');
+    }
+  }
+);
+
+// Clean up the tokens that are no longer valid
+function cleanupTokens(response, tokens) {
+  // For each notification we check if there was an error
+  const tokensDelete = [];
+  response.results.forEach((result, index) => {
+    const error = result.error;
+    if (error) {
+      console.error('Failing sending notification to', tokens[index], error);
+      // Clean up the tokens who are not registered anymore.
+      if (error.code === 'messaging/invalid-registration-token' ||
+          error.code === 'messaging/registration-token-not-registered') {
+        const deleteTask = admin.firestore().collection('messages').doc(tokens[index]).delete();
+        tokensDelete.push(deleteTask);
+      }
+    }
+  });
+  return Promise.all(tokensDelete);
+}
